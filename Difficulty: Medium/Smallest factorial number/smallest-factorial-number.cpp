17cd421b4016b19ce113a38199da16f5feeff08b
Amitrahar728 <amitrahar728@gmail.com>
2025-04-26 22:29:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Smallest factorial number/smallest-factorial-number.cpp
+++ Difficulty: Medium/Smallest factorial number/smallest-factorial-number.cpp
@@ -4,37 +4,29 @@ using namespace std;
 
 // } Driver Code Ends
 
-class Solution
-{
-    public:
-        int trailingZeroes(int x) {
-        int count = 0;
-        int factor = 5;
-        while (x >= factor) {
-            count += x / factor;
-            factor *= 5;
+
+class Solution {
+  public:
+    int findNum(int n) {
+         int low = 0, high = 5 * n, ans = -1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        int count = 0, temp = mid;
+        while (temp > 0) {
+            count += temp / 5;
+            temp /= 5;
         }
-    return count;
-    }
-        int findNum(int n)
-        {
-            if (n == 0) return 0;
-        int low = 0, high = 5 * n;
-        int result = 0;
-    
-        while (low < high) {
-            int mid = low + (high - low) / 2; 
-            if (trailingZeroes(mid) >= n) {
-                 high = mid; 
-        } 
-            else {
-                low = mid + 1; 
+        if (count >= n) {
+            ans = mid;
+            high = mid - 1;
+        } else {
+            low = mid + 1;
         }
     }
-    
-    return low; 
+    return ans;
     }
 };
+
 
 //{ Driver Code Starts.
 
