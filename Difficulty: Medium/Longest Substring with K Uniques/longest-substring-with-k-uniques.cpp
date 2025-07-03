fe180bebbbf657379134736b046f1a2d9ded7c1f
Amitrahar728 <amitrahar728@gmail.com>
2025-07-03 08:58:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Substring with K Uniques/longest-substring-with-k-uniques.cpp
+++ Difficulty: Medium/Longest Substring with K Uniques/longest-substring-with-k-uniques.cpp
@@ -1,54 +1,34 @@
-//{ Driver Code Starts
-// Initial template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
-// User function template for C++
-
 class Solution {
   public:
     int longestKSubstr(string &s, int k) {
-        int l =0; int r =0; int maxlen =-1; 
-        unordered_map<char,int> mpp;
-        while(r<s.size()){
-            mpp[s[r]]++;
-            if(mpp.size()>k){
-                mpp[s[l]]--;
-                if(mpp[s[l]] == 0){
-                    mpp.erase(s[l]);
-                }
-                l++;
-            }
-            if(mpp.size()==k){
-                maxlen = max(maxlen, r-l+1);
-            }
-            r++;
+        int n = s.size();
+        int i =0;
+        int j =0;
+        int maxi = -1;
+        int cnt =0;
+        vector<int> fre(26,0);
+        while(j<n){
+            
+        fre[s[j] - 'a']++;
+        
+        if (fre[s[j] - 'a'] == 1) cnt++;
+
+        while (cnt > k) {
+            fre[s[i] - 'a']--;
+          
+            if (fre[s[i] - 'a'] == 0) cnt--;  
+            i++;
         }
-        return maxlen == 0 ? -1 : maxlen;
+
+        if (cnt == k) {
+            maxi = max(maxi, j - i + 1);
+        }
+
+        j++;
+        }
+        
+        return maxi;
+        
         
     }
 };
-
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        string s;
-        cin >> s;
-        int k;
-        cin >> k;
-        Solution ob;
-        cout << ob.longestKSubstr(s, k) << endl;
-
-        cout << "~"
-             << "\n";
-    }
-}
-
-// } Driver Code Ends
